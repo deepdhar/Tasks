f965e7fcbeae1701dd34770e54e8e8408079aff0
deepdhar <dhar2017.slg@gmail.com>
2021-04-07 23:44:07 +0530

    updated files

---
+++ app/src/main/java/com/example/android/todolist/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.android.todolist;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
