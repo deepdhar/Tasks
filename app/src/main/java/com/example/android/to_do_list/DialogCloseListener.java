5bf34d9071837c9010551a6125b20b5ad50183d7
deepdhar <dhar2017.slg@gmail.com>
2022-03-06 10:09:21 +0530

    cleared unnecessasry files

--- app/src/main/java/com/example/android/to_do_list/DialogCloseListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.android.to_do_list;
-
-import android.content.DialogInterface;
-
-public interface DialogCloseListener {
-    public void handleDialogClose(DialogInterface dialog);
-}
--- app/src/main/java/com/example/android/todolist/DialogCloseListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.android.todolist;
-
-import android.content.DialogInterface;
-
-public interface DialogCloseListener {
-    public void handleDialogClose(DialogInterface dialog);
-}
